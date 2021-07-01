6daac72cb5d2004191912f9307fe0bad588a8f65
Lionel Médini <lionel.medini@univ-lyon1.fr>
2021-07-02 00:04:19 +0200

    Version SPA avec AJAX - finale 01/07/2021

--- public/client.js
+++ public/client.js
@@ -1,6 +1,33 @@
+/********************/
+// GESTION DE LA SPA
+/********************/
+
+/**
+ * Fonction qui affiche une nouvelle vue sur le client
+ * @param {*} hash correspondant à l'id de l'élément à afficher
+ */
 function show(hash) {
     $('.active').removeClass('active').addClass('inactive');
     $(hash).removeClass('inactive').addClass('active');
 }
 
-window.addEventListener('hashchange',() => show(window.location.hash));
+// Abonnement à l'événement de changement de hash
+window.addEventListener('hashchange', () => show(window.location.hash));
+
+/********************/
+// AJAX
+/********************/
+
+function appelleDisBonjour() {
+    fetch('/disbonjour-json', {
+        method: "POST",
+        body: '{"prenom": "' + document.getElementById("prenom").value + '"}',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    ).then(res => res.json()).then(json => {
+        document.getElementById("prenom-placeholder").innerHTML = json.message;
+        window.location.hash = "#bonjour-view";
+    });
+}
